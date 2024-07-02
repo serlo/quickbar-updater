ebcf5295df7867ecae3fb4a871a9821cf8ba956c
Botho <1258870+elbotho@users.noreply.github.com>
2024-07-02 12:39:08 +0200

    use newer regex to make it work for new coursePage aliases

--- src/2-extract.ts
+++ src/2-extract.ts
@@ -17,12 +17,16 @@ const extractIdFromPath = (path) => {
   // Remove protocol and domain
   const cleanedPath = path.replace(/^https?:\/\/de.serlo.org/, '');
   // Original regular expression for matching ID patterns in the path
-  const reg = /^(?:(?:\/[^\/]+)?\/([\d]+)\/[^\/]+|\/([\d]+)|\/taxonomy\/term\/get\/([\d]+))$/;
-  const match = reg.exec(cleanedPath);
+  const reg =
+    /^\/(?<subject>[\w-]+\/)?(?<id>\d+)(?<coursePageId>\/[0-9a-f]+)?\/(?<title>[^/]*)$/;
+
+  const simpleReg = /^\/(?<id>\d+)$/;
+  const match = reg.exec(cleanedPath) ?? simpleReg.exec(cleanedPath);
 
   // Parse the ID from regex match groups
-  if (match) {
-    return parseInt(match[1] || match[2] || match[3], 10);
+  if (match && match.groups?.id) {
+    const id = parseInt(match.groups.id);
+    if (!isNaN(id)) return id;
   }
 
   // Attempt to resolve ID using the resolver mapping
